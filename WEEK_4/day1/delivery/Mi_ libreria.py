2193cd752c01ebceed05c90dab6c24cc1d7b8816
Filipate <filipa.teles@icloud.com>
2020-07-20 21:12:06 +0200

    actualizaciones

--- WEEK_4/day1/delivery/Mi_ libreria.py
+++ WEEK_4/day1/delivery/Mi_ libreria.py
@@ -4,4 +4,32 @@ import panda as pd
 import numpy as np
 import jason 
 
+def _f_protected():
+    #Función que recibe 1 entero y devuelve True si el valor es mayor que 5, y False en caso contrario
+    l1 = []
+    funcion = lambda x : x >5,l1
+    for elem in range(16):
+        l1.append(elem)
+    return list(filter(funcion, l1))
+
+filter_ = _f_protected()
+print(filter_)
+
+def _f_protected():
+    #Función que recibe 1 entero y devuelve True si el valor es mayor que 5, y False en caso contrario
+    funcion = lambda x: x > 5
+    l1 = [x for x in range(16)]#list_comprehension
+    l1_filtrada = list(filter(funcion, l1))
+    return l1_filtrada
+
+filter_ = _f_protected()
+print(filter_)
     
+
+def _f_protected():
+    l1 = [x for x in range(16)]
+    l1_filtrada = list(filter(lambda x: x >5, l1))
+    return l1_filtrada
+
+filter_ = _f_protected()
+print(filter_)
